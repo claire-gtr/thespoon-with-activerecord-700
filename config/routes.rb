d31290ee553965ebf4cbb8de8ae0d3595933afec
claire gautier <claire.gautier@edhec.com>
2021-08-17 11:08:56 +0200

    finished lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # See all restaurants
+  # get '/restaurants', to: 'restaurants#index'
+
+  # # Create a restaurant
+  #   # display form
+  #   get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  #   # send information to DB
+  #   post '/restaurants', to: 'restaurants#create'
+
+  # # See details about one restaurant
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  
+  # # Update a restaurant
+  #   # display pre filled from
+  #   get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  #   # update information in DB
+  #   patch '/restaurants/:id', to: 'restaurants#update'
+
+  # # Destroy a restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy', as: :delete_restaurant
+
+  resources :restaurants, except: [:index, :show]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+puts "Destroyed all the restaurants"
+puts "Creating restaurants...."
+Restaurant.create(name: "Mamma Primi", rating: 4)
+Restaurant.create(name: "L'entrecote", rating: 5)
+Restaurant.create(name: "Le regent", rating: 3)
+Restaurant.create(name: "Sushi samba", rating: 3)
+Restaurant.create(name: "Dishoom", rating: 3)
+puts "Created restaurants!"
